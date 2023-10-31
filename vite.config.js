2ab1b96f1f1303c338732d507fda115be14696d3
joey <2390923149@qq.com>
2023-10-31 16:42:33 +0800

    fix: out Dir

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'zikao-vue-client',
+    outDir: 'dist',
   },
   css: {
     postcss: {
