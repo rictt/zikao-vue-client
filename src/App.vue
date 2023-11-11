0397b68893a4bc06ff285971e42c8593d5318dc2
joey <2390923149@qq.com>
2023-11-11 19:38:08 +0800

    fix: 备案

--- src/App.vue
+++ src/App.vue
@@ -54,19 +54,26 @@ const goHome = () => {
         </div>
         <div class="flex items-center">
           <img src="./assets/image/qq.png" class="w-[84px] scale-[1.2]" alt="">
-          <a class="pl-6 text-sm text-[#666] underline py-2" target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=Gu02gzg0safRMKxEKJtOkrIoiOq9l0fz&jump_from=webapi&authKey=hwsMVQ5VrjfzONDNXvINyqc2keY6hQKh2mO46LUYqGc313Q9Y6wsXkv8CXqXIoZS">
+          <a class="pl-6 text-sm text-[#666] underline py-2" target="_blank"
+            href="https://qm.qq.com/cgi-bin/qm/qr?k=Gu02gzg0safRMKxEKJtOkrIoiOq9l0fz&jump_from=webapi&authKey=hwsMVQ5VrjfzONDNXvINyqc2keY6hQKh2mO46LUYqGc313Q9Y6wsXkv8CXqXIoZS">
             <img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="自考365学习|交流群" title="自考365学习|交流群">资料｜反馈群
           </a>
         </div>
       </header>
     </div>
-    <div class="pc-container py-4">
+    <div class="pc-container py-4 min-h-[70vh]">
       <RouterView v-slot="{ Component }">
         <KeepAlive :include="['Home']">
           <component :is="Component" />
         </KeepAlive>
       </RouterView>
     </div>
+  </div>
+  <div class="py-[10px] w-full fixed text-center bottom-0">
+    <footer class="text-center py-[4px]">
+      <a href="https://beian.miit.gov.cn/" target="_blank">ICP备案号：</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023128276号</a>
+    </footer>
   </div>
 </template>
 
